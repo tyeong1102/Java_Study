5cff44ff3744859d7d4ad61db234d7a45e794a6d
Kim Taeyeong <tyeong1102@gmail.com>
2024-01-31 01:05:46 +0900

    상속

---
+++ java-basic/src/extends1/overriding/CarMain.java
@@ -0,0 +1,14 @@
+package extends1.overriding;
+
+import extends1.ex1.GasCar;
+
+public class CarMain {
+
+    public static void main(String[] args) {
+        ElectricCar electricCar = new ElectricCar();
+        electricCar.move();
+
+        GasCar gasCar = new GasCar();
+        gasCar.move();
+    }
+}
