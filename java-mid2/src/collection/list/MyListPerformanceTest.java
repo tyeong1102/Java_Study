cde2be434da47df630744445f9de5015c74e5f15
Taeyeong Kim <tyeong1102@gmail.com>
2025-07-02 17:49:53 +0900

    리스트 성능비교

---
+++ java-mid2/src/collection/list/MyListPerformanceTest.java
@@ -0,0 +1,23 @@
+package collection.list;
+
+public class MyListPerformanceTest {
+
+    public static void main(String[] args) {
+        int size = 50_000;
+        System.out.println("==MyArrayList 추가==");
+        addFirst(new MyArrayList<>(), size);
+
+        System.out.println("==MyLinkedList 추가==");
+        addFirst(new MyLinkedList<>(), size);
+    }
+
+    private static void addFirst(MyList<Integer> list, int size) {
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < size; i++) {
+            list.add(i);
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("앞에 추가 - 크기: " + size + ", 계산 시간: " + (endTime - startTime) + "ms");
+
+    }
+}
